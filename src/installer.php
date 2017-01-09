7334f7d886506c91c3db3d4d835a9e103984737f
Rodrigo Manara <rodrigomanara@yahoo.co.uk>
2017-01-09 08:02:01 +0000

    test install on from vendor

--- src/installer.php
+++ src/installer.php
@@ -5,27 +5,26 @@ namespace ComposerScript;
 use IDE\Model;
 use IDE\System;
 use IDE\Writer;
-use Composer\Script\Event;
 
 class Installer {
 
 
     public static function Init() {
-        
+
         $model = new Model();
 
         $result_model = $model->setPath("upload\\admin\\model")
-                ->setPath( "upload\\catalog\\model")
+                ->setPath("upload\\catalog\\model")
                 ->init();
 
         $engine = new System();
-        $result_negine = $engine->setPath( "upload\\system\\library")
+        $result_negine = $engine->setPath("upload\\system\\library")
                 ->init();
 
         $default = array(
-            array('method'=> 'Loader' , 'type' => '$load'),
+            array('method' => 'Loader', 'type' => '$load'),
         );
-        $merge = array_merge($result_model, $result_negine , $default);
+        $merge = array_merge($result_model, $result_negine, $default);
 
 
         new Writer($merge, "upload\\system\\engine\\controller.php");
