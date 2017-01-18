1181b9c266c80eb3afae6516c335f898189a4385
Rodrigo Manara <rodrigomanara@yahoo.co.uk>
2017-01-18 09:27:04 +0000

    add fix for pushing wrong folders

---
+++ src/IDE/Model.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace IDE;
+/**
+ * @author Rodrigo Manara <me@rodrigomanara.co.uk>
+ */
+
+Class Model extends AbstractBuilder implements InterfaceBuilder{
+
+    public function init() {
+
+        //find all files
+        $this->getFiles();
+
+        $build_list = array();
+        foreach ($this->path as $path) {
+             
+          $build_list[] = $this->getModel($this->getClassName($this->getContent($path)) , $path);
+            
+        } 
+        return $build_list;
+    }
+
+}
