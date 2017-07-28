9f01175dae54e95827834182940adb3377f437c3
LAPTOP-PLVVCJSK\Rodrigo <rodrigomanara@yahoo.co.uk>
2017-07-28 20:33:30 +0100

    fix issue on writer, it was appending and not rewritten between php tag and abstract

--- src/IDE/Writer.php
+++ src/IDE/Writer.php
@@ -3,10 +3,12 @@
 namespace IDE;
 
 use Symfony\Component\Filesystem\Filesystem;
+
 /**
  * @author Rodrigo Manara <me@rodrigomanara.co.uk>
  */
 Class Writer {
+
     /**
      * 
      * @param array $list
@@ -15,16 +17,15 @@ Class Writer {
     public function __construct(array $list, $path) {
 
         $content = file_get_contents($path);
-        #$count = strlen($content);
- 
+       
         $phpdoc = $this->property($list);
         $phpdoc = explode("\n", $phpdoc);
         $phpdoc = array_unique($phpdoc);
-        $phpdoc = implode(PHP_EOL,$phpdoc);
+        $phpdoc = implode(PHP_EOL, $phpdoc);
 
-        $new_content = str_replace("<?php", "<?php".PHP_EOL.$phpdoc, $content);
+        $explode = explode("abstract", $content);
+        $new_content = "<?php " . $phpdoc .  " abstract" . $explode[1];
 
-                
         try {
             $file = new Filesystem();
             $file->dumpFile($path, $new_content);
@@ -32,23 +33,24 @@ Class Writer {
             var_dump($e);
         }
     }
+
     /**
      * Build an array of property
      * @param array $values
      * @return type
      */
-    private function property(array $values){
-        
+    private function property(array $values) {
+
         $append = array();
-        
-        
-        $append[] = sprintf( "%s \t/**%s", PHP_EOL, PHP_EOL );
-        foreach($values as $value){
-            array_push($append , sprintf( "\t* @property %s %s %s", $value['method'] , $value['type'], PHP_EOL ));
+
+
+        $append[] = sprintf("%s \t/**%s", PHP_EOL, PHP_EOL);
+        foreach ($values as $value) {
+            array_push($append, sprintf("\t* @property %s %s %s", $value['method'], $value['type'], PHP_EOL));
         }
-        $append[] = sprintf( "\t**/%s", PHP_EOL );
-        
-        return implode("" , $append); 
+        $append[] = sprintf("\t**/%s", PHP_EOL);
+
+        return implode("", $append);
     }
-    
+
 }
